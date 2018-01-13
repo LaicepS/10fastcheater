32eec630340e47a7d71dec9e023ca39abbc37859
Dorian <dorian.haglund@gmail.com>
2018-01-13 01:49:56 +0100

    Basic script which does the job

--- sel0.py
+++ sel0.py
@@ -1,4 +1,15 @@
 from selenium import webdriver
+from time import sleep
 
-brower = webdriver.Firefox()
-brower.get("https://10fastfingers.com/typing-test/french")
+browser = webdriver.Firefox()
+browser.get("https://10fastfingers.com/typing-test/french")
+wordlist = browser.find_elements_by_css_selector("[wordnr]")
+words = map(lambda x: x.text, wordlist)
+input = browser.find_element_by_id("inputfield")
+
+for w in words:
+    for c in w:
+        input.send_keys(c)
+        sleep(0.05)
+    from selenium.webdriver.common.keys import Keys
+    input.send_keys(Keys.SPACE)
